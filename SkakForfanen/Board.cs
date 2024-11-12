e72c854043efbdb4dc38f6cc669c447d3a285c81
CatoTheSlayer <iancatohansen@gmail.com>
2024-11-12 09:30:58 +0100

    create 2d array
    lasse din mor

---
+++ SkakForfanen/Board.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkakForfanen
+{
+    internal class Board
+    {
+        // Ian: skal muligvis være en anden array type
+        int[,] boards =
+            {
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+                {1, 2, 3, 4, 5, 6, 7, 8},
+        };
+    }
+}
